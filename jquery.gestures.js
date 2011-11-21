6eb3a32b234cf49054d1a1b7af0112b59b9e8e91
Nicolas Gryman <ngryman@gmail.com>
2011-11-21 16:31:23 +0100

    use of 'mouseleave' instead of 'mouseout' event to end gesture when user leaves the element.

--- jquery.gestures.js
+++ jquery.gestures.js
@@ -6,47 +6,47 @@
  */
 
 (function($) {
-  var touchSupported = ('ontouchstart' in window);
-  var startEvent = touchSupported ? 'touchstart' : 'mousedown';
-  var moveEvent  = touchSupported ? 'touchmove'  : 'mousemove';
-  var endEvent = touchSupported ? 'touchend' : 'mouseup mouseout';
-  var gestures = ['swipe'];
-  var NS = 'gestures' + Math.ceil(Math.random() * Math.pow(10, 10));
-  var updateGestures = {};
+	var touchSupported = ('ontouchstart' in window);
+	var startEvent = touchSupported ? 'touchstart' : 'mousedown';
+	var moveEvent = touchSupported ? 'touchmove' : 'mousemove';
+	var endEvent = touchSupported ? 'touchend' : 'mouseup mouseleave';
+	var gestures = ['swipe'];
+	var NS = 'gestures' + Math.ceil(Math.random() * Math.pow(10, 10));
+	var updateGestures = {};
 
 
-  function getCoordinates(event) {
-    return  {
-      x: touchSupported ?
-        event.originalEvent.touches[0].pageX :
-        event.clientX + (document.body.scrollLeft || document.documentElement.scrollLeft),
-      y: touchSupported ?
-        event.originalEvent.touches[0].pageY :
-        event.pageY + (document.body.scrollTop  || document.documentElement.scrollTop)
-    };
-  }
+	function getCoordinates(event) {
+		return	{
+			x: touchSupported ?
+				event.originalEvent.touches[0].pageX :
+				event.clientX + (document.body.scrollLeft || document.documentElement.scrollLeft),
+			y: touchSupported ?
+				event.originalEvent.touches[0].pageY :
+				event.pageY + (document.body.scrollTop || document.documentElement.scrollTop)
+		};
+	}
 
 
-  function createEvent(event, data) {
-    if (!data) data = {};
-    data.originalEvent = event;
-    return data;
-  }
+	function createEvent(event, data) {
+		if (!data) data = {};
+		data.originalEvent = event;
+		return data;
+	}
 
 
-  function createData(el, gestureName) {
-    var opts = el[NS],
-      gestureOpts = el[NS].gestures[gestureName];
+	function createData(el, gestureName) {
+		var opts = el[NS],
+			gestureOpts = el[NS].gestures[gestureName];
 
-    // first move, stores old coordinates
-    if (gestureOpts.data === undefined) {
-      gestureOpts.data = {};
-      gestureOpts.data.x = opts.x;
-      gestureOpts.data.y = opts.y;
-    }
+		// first move, stores old coordinates
+		if (gestureOpts.data === undefined) {
+			gestureOpts.data = {};
+			gestureOpts.data.x = opts.x;
+			gestureOpts.data.y = opts.y;
+		}
 
-    return gestureOpts.data;
-  }
+		return gestureOpts.data;
+	}
 
 
 	function createGesture(el, gestureName) {
@@ -57,9 +57,9 @@
 
 		if (gestureOpts[gestureName] === undefined) {
 			gestureOpts[gestureName] = {
-			name:   gestureName,
-      update: $.Callbacks(),
-      end:    $.Callbacks()
+				name:	 gestureName,
+				update: $.Callbacks(),
+				end:		$.Callbacks()
 			};
 		}
 
@@ -67,142 +67,142 @@
 	}
 
 
-  function onMove(event) {
-    var el = this,
-      opts = el[NS];
+	function onMove(event) {
+		var el = this,
+			opts = el[NS];
 
-    // if not moving, discard it
-    if (!opts.isMoving) return;
+		// if not moving, discard it
+		if (!opts.isMoving) return;
 
-    $.each(opts.gestures, function() {
-      var customEvent = updateGestures[this.name].call(el, event, getCoordinates(event));
+		$.each(opts.gestures, function() {
+			var customEvent = updateGestures[this.name].call(el, event, getCoordinates(event));
 			this.update.fireWith(el, [customEvent])
-    });
-  }
+		});
+	}
 
 
-  function onEnd(event) {
-    var el = this,
-      opts = el[NS], e;
+	function onEnd(event) {
+		var el = this,
+			opts = el[NS], e;
 
-    // stop move, reset for next event
-    delete opts.isMoving;
-    delete opts.x;
-    delete opts.y;
+		// stop move, reset for next event
+		delete opts.isMoving;
+		delete opts.x;
+		delete opts.y;
 
-    // unregisters on the fly events
-    $(this).unbind(moveEvent).unbind(endEvent);
+		// unregisters on the fly events
+		$(this).unbind(moveEvent).unbind(endEvent);
 
-    $.each(opts.gestures, function() {
-      e = createEvent(event, this.data);
-      delete this.data;
+		$.each(opts.gestures, function() {
+			e = createEvent(event, this.data);
+			delete this.data;
 			this.end.fireWith(el, [e]);
-    });
-  }
+		});
+	}
 
 
-  function registerCallbacks(el, gestureName, updateCallback, endCallback) {
-    var $el = $(el);
+	function registerCallbacks(el, gestureName, updateCallback, endCallback) {
+		var $el = $(el);
 
-    if (!$el.prop(NS))
-      $el.prop(NS, {});
+		if (!$el.prop(NS))
+			$el.prop(NS, {});
 
-    var opts = $el.prop(NS);
+		var opts = $el.prop(NS);
 
-    // stores callbacks
+		// stores callbacks
 		var gestures = createGesture(el, gestureName);
 		if (typeof updateCallback == 'function') gestures.update.add(updateCallback);
-		if (typeof endCallback    == 'function') gestures.end.add(endCallback);
+		if (typeof endCallback == 'function') gestures.end.add(endCallback);
 
-    // if start is already registered, skip this
-    if (opts.x && opts.y) return;
+		// if start is already registered, skip this
+		if (opts.x && opts.y) return;
 
-    $el.bind(startEvent, function(event) {
-      var $this = $(this);
-      var opts =  $this.prop(NS);
+		$el.bind(startEvent, function(event) {
+			var $this = $(this);
+			var opts = $this.prop(NS);
 
-      // is moving
-      opts.isMoving = true;
+			// is moving
+			opts.isMoving = true;
 
-      // remembers start coordinates
-      $.extend(opts, getCoordinates(event));
+			// remembers start coordinates
+			$.extend(opts, getCoordinates(event));
 
-      // call gesture update function and, if defined, user <xxx>change event callback
-      $this.bind(moveEvent, onMove);
-      // end gesture
-      $this.bind(endEvent, onEnd);
-    });
-  }
-
-
-  /*$.fn.gestures = function(options) {
-    var opts = {
-      cancelOnMove: true,
-      onlyIf: function() { return true },
-      touchDelay: 0,
-      callback: undefined
-    };
-
-    switch(typeof options) {
-      case 'function':
-        opts.callback = options;
-        break;
-
-      case 'object':
-        $.extend(opts, options);
-        break;
-    }
-
-    return this;
-  };*/
+			// call gesture update function and, if defined, user <xxx>change event callback
+			$this.bind(moveEvent, onMove);
+			// end gesture
+			$this.bind(endEvent, onEnd);
+		});
+	}
 
 
-  /*
-   * swipe gesture
-   */
+	/*$.fn.gestures = function(options) {
+	 var opts = {
+	 cancelOnMove: true,
+	 onlyIf: function() { return true },
+	 touchDelay: 0,
+	 callback: undefined
+	 };
 
-  updateGestures.swipe = function(event, coordinates) {
-    var e = createEvent(event),
-      data = createData(this, 'swipe');
+	 switch(typeof options) {
+	 case 'function':
+	 opts.callback = options;
+	 break;
 
-    // computes deltas
-    var dX = coordinates.x - data.x;
-    var dY = coordinates.y - data.y;
+	 case 'object':
+	 $.extend(opts, options);
+	 break;
+	 }
 
-    // first move, detection which direction
-    if (!data.direction)
-      data.direction = Math.abs(dX) > Math.abs(dY) ? 'horizontal' : 'vertical';
-
-    // sets direction and selects the right delta
-    e.direction = data.direction;
-    e.delta = data.direction == 'horizontal' ? dX : dY;
-
-    // accumulates deltas
-    if (!data.delta)
-      data.delta = 0;
-    data.delta += e.delta;
-
-    // stores old coordinates
-    data.x = coordinates.x;
-    data.y = coordinates.y;
-
-    return e;
-  };
+	 return this;
+	 };*/
 
 
-  $.fn.swipechange = function(callback) {
-    this.each(function() {
-      registerCallbacks(this, 'swipe', callback);
-    });
-    return this;
-  };
+	/*
+	 * swipe gesture
+	 */
+
+	updateGestures.swipe = function(event, coordinates) {
+		var e = createEvent(event),
+			data = createData(this, 'swipe');
+
+		// computes deltas
+		var dX = coordinates.x - data.x;
+		var dY = coordinates.y - data.y;
+
+		// first move, detection which direction
+		if (!data.direction)
+			data.direction = Math.abs(dX) > Math.abs(dY) ? 'horizontal' : 'vertical';
+
+		// sets direction and selects the right delta
+		e.direction = data.direction;
+		e.delta = data.direction == 'horizontal' ? dX : dY;
+
+		// accumulates deltas
+		if (!data.delta)
+			data.delta = 0;
+		data.delta += e.delta;
+
+		// stores old coordinates
+		data.x = coordinates.x;
+		data.y = coordinates.y;
+
+		return e;
+	};
 
 
-  $.fn.swipeend = function(callback) {
-    this.each(function() {
-      registerCallbacks(this, 'swipe', null, callback);
-    });
-    return this;
-  };
+	$.fn.swipechange = function(callback) {
+		this.each(function() {
+			registerCallbacks(this, 'swipe', callback);
+		});
+		return this;
+	};
+
+
+	$.fn.swipeend = function(callback) {
+		this.each(function() {
+			registerCallbacks(this, 'swipe', null, callback);
+		});
+		return this;
+	};
 
 })(jQuery);
